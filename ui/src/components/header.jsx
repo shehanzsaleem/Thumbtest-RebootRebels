903544abc345112928b0399a4ce629b3fc5146ba
zlogonje-pissa <56260108+zlogonje-pissa@users.noreply.github.com>
2021-01-26 17:46:24 +0530

    dasasd

---
+++ ui/src/components/header.jsx
@@ -0,0 +1,22 @@
+ import React, { Component } from 'react';
+
+ 
+import logo from '../images/Logo/Logo-02.png';
+import '../App.css';
+
+
+ class Header extends Component {
+     state = {  }
+     render() { 
+         return ( 
+            <header className="topnav" id="myTopnav">
+            <a href="#home">
+              <img id="logo" src={logo} />
+            </a>
+    
+          </header>
+          );
+     }
+ }
+  
+ export default Header;
--- ui/src/index.js
+++ ui/src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Header from './components/header';
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
