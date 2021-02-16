53257ff99d4e5ba29e2fef5a8abfcedbd3ad4588
zlogonje-pissa <56260108+zlogonje-pissa@users.noreply.github.com>
2021-02-17 00:43:20 +0530

    Up

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,95 +1,96 @@
-// import logo from './logo.svg';
-import logo from './images/Logo/Logo-02.png';
-import './App.css';
+import React, { useState, useRef, useReducer } from "react";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import "./App.css";
+
+const machine = {
+  initial: "initial",
+  states: {
+    initial: { on: { next: "loadingModel" } },
+    loadingModel: { on: { next: "modelReady" } },
+    modelReady: { on: { next: "imageReady" } },
+    imageReady: { on: { next: "identifying" }, showImage: true },
+    identifying: { on: { next: "complete" } },
+    complete: { on: { next: "modelReady" }, showImage: true, showResults: true }
+  }
+};
 
 function App() {
+  const [results, setResults] = useState([]);
+  const [imageURL, setImageURL] = useState(null);
+  const [model, setModel] = useState(null);
+  const imageRef = useRef();
+  const inputRef = useRef();
+
+  const reducer = (state, event) =>
+    machine.states[state].on[event] || machine.initial;
+
+  const [appState, dispatch] = useReducer(reducer, machine.initial);
+  const next = () => dispatch("next");
+
+  const loadModel = async () => {
+    next();
+    const model = await mobilenet.load();
+    setModel(model);
+    next();
+  };
+
+  const identify = async () => {
+    next();
+    const results = await model.classify(imageRef.current);
+    setResults(results);
+    next();
+  };
+
+  const reset = async () => {
+    setResults([]);
+    next();
+  };
+
+  const upload = () => inputRef.current.click();
+
+  const handleUpload = event => {
+    const { files } = event.target;
+    if (files.length > 0) {
+      const url = URL.createObjectURL(event.target.files[0]);
+      setImageURL(url);
+      next();
+    }
+  };
+
+  const actionButton = {
+    initial: { action: loadModel, text: "Load Model" },
+    loadingModel: { text: "Loading Model..." },
+    modelReady: { action: upload, text: "Upload Image" },
+    imageReady: { action: identify, text: "Identify Breed" },
+    identifying: { text: "Identifying..." },
+    complete: { action: reset, text: "Reset" }
+  };
+
+  const { showImage, showResults } = machine.states[appState];
+
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <div className="App">
-
-      <header className="topnav" id="myTopnav">
-        <a href="#home">
-          <img id="logo" src={logo} />
-        </a>
-
-      </header>
-
-
-      <div className="container app-main" style="background-color: #2f2e33;">
-
-        <div className="row">
-          <div className="col-12">
-            <div className="title-banner">
-              <div className="center">
-                <h3><span className="icon"></span>Test Your Youtube Thumnail</h3>
-                <h6 className="thin">Score Your Youtube Thumnail based on Machine Learning</h6>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-
-          <div className="col-12">
-            <div className="box">
-              <div className="inner-box">
-                <div className="inner-box-content center">
-                  <br />
-                  <input type="file" id="myFile" name="filename" className="button" />
-
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-        <div className="row">
-          <div className="col-md-4 col-12">
-            <div className="info-box">
-
-              <h5>Powered by Machine Learning</h5>
-              <p className="small thin">Score Your Youtube Thumnail based on Machine Learning</p>
-            </div>
-          </div>
-          <div className="col-md-4 col-12">
-            <div className="info-box">
-
-              <h5>The Ultimate Thumnail Tool for Youtubers</h5>
-              <p className="small thin">Score Your Youtube Thumnail based on Machine Learning</p>
-            </div>
-          </div>
-          <div className="col-md-4 col-12">
-            <div className="info-box">
-
-              <h5>Score your Thumbnail in Seconds</h5>
-              <p className="small thin">Score Your Youtube Thumnail based on Machine Learning</p>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-   
-   
+    <div>
+      {showImage && <img src={imageURL} alt="upload-preview" ref={imageRef} />}
+      <input
+        type="file"
+        accept="image/*"
+        capture="camera"
+        onChange={handleUpload}
+        ref={inputRef}
+      />
+      {showResults && (
+        <ul>
+          {results.map(({ className, probability }) => (
+            <li key={className}>{`${className}: %${(probability * 100).toFixed(
+              2
+            )}`}</li>
+          ))}
+        </ul>
+      )}
+      <button onClick={actionButton[appState].action || (() => {})}>
+        {actionButton[appState].text}
+      </button>
     </div>
-
-
-
   );
 }
 
