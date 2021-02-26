807e6d5fe5780d9ead85241ee0beb4bcaee08990
samithakalpana <66740752+samithakalpana@users.noreply.github.com>
2021-02-26 17:16:40 +0530

    testing

--- ui/src/components/pages/Score.js
+++ ui/src/components/pages/Score.js
@@ -5,7 +5,7 @@ function Score(){
     return(
 
 <div>
-    <h1>This the page where the Scores are displayed (To be designed)</h1>
+    <h1>This the page where the Scores are displayed (To be designed soon)</h1>
 </div>
 
 
