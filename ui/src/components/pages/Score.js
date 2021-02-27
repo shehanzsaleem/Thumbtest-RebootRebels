2a9b9e0e494a9ea9dc3de648e32732395b3f16d0
samithakalpana <66740752+samithakalpana@users.noreply.github.com>
2021-02-27 23:14:09 +0530

    minor updates

--- ui/src/components/pages/Score.js
+++ ui/src/components/pages/Score.js
@@ -11,7 +11,7 @@ function Score(){
       <HeroSection {...homeObjThree} />
       <HeroSection {...homeObjTwo} />
       {/* <Pricing /> */}
-      <HeroSection {...homeObjFour} />
+      {/* <HeroSection {...homeObjFour} /> */}
     </>
 
 
