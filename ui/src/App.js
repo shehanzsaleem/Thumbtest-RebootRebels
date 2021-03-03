ce7d185aa6d347d69322e88c28ab906a3c9f7755
zlogonje-pissa <56260108+zlogonje-pissa@users.noreply.github.com>
2021-03-03 05:30:18 +0530

    Installed Tensorflow, Imported Model
    Tensorflow was imported to the react app and The Model.json file is in the Public Folder

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,22 +1,74 @@
-
+import React, { useState, useCallback } from 'react';
 import{Route, Link} from 'react-router-dom';
+// import React from "react"; 
+
+import * as tf from '@tensorflow/tfjs';
+import {loadGraphModel} from '@tensorflow/tfjs-converter';
+
+
 import './App.css';
 // import './components/Home';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from "react"; 
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Score from './components/pages/Score';
 import Contact from './components/pages/Contact';
 import Navbar from './components/layouts/Navbar';
 
-function App() {
+const MODEL_URL = process.env.PUBLIC_URL + '/model/';
+const LABELS_URL = MODEL_URL + 'labels.json';
+const MODEL_JSON = MODEL_URL + 'model.json';
+
+const App = () => {
+
+  const [model, setModel] = useState(null);
+  const [labels, setLabels] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [selected, setSelected] = useState('');
+  const fetchModel = useCallback((model) => {
+      setModel(model);
+  }, []);
+
+  const fetchLabels = useCallback((labels) => {
+      setLabels(labels);
+  }, []);
+
+  const selectMode = useCallback((selected) => {
+      setSelected(selected)
+  }, [])
+
+  const loadModel = async () => {
+    setLoading(true);
+    const model = await tf.loadGraphModel(MODEL_JSON);
+    fetchModel(model);
+    const response = await fetch(LABELS_URL);
+    let labels = await response.json();
+    fetchLabels(labels)
+    setLoading(false);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
 
     
-
-
-
     <div className="App">
       <Navbar/>
      <Route exact path="/" component={Score}/>
