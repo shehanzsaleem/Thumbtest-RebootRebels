9e560d44647c7f2d7b9f2ffe3a9a7acad2b9d386
zlogonje-pissa <56260108+shehanzsaleem@users.noreply.github.com>
2021-03-17 04:13:11 +0530

    Bug Fix

--- ui/src/components/layouts/HeroSection.js
+++ ui/src/components/layouts/HeroSection.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 function HeroSection({
-lightBg,
-topLine,
-lightText,
-lightTextDesc,
-headline,
-description,
-buttonLabel,
-img,
-alt,
-imgStart })
-
-{
+  lightBg,
+  topLine,
+  lightText,
+  lightTextDesc,
+  headline,
+  description,
+  buttonLabel,
+  img,
+  alt,
+  imgStart }) {
 
   return (
     <div className={lightBg ? 'home__hero-section' : 'home__hero-section darkBg'} >
       <div className='container'>
 
-        <div className='row home__hero-row' style={{ display: 'flex',
-flexDirection: imgStart === 'start' ? 'row-reverse' : 'row',
-}} >
+        <div className='row home__hero-row' style={{
+          display: 'flex',
+          flexDirection: imgStart === 'start' ? 'row-reverse' : 'row',
+        }} >
 
           <div className='col'>
             <div className='home__hero-text-wrapper'>
@@ -32,7 +31,7 @@ flexDirection: imgStart === 'start' ? 'row-reverse' : 'row',
               <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}
               > {description}
               </p>
-              <Link to='./Home'>
+              <Link to='./Model'>
                 <button type="button" className="btn btn-outline-secondary ml-center">Begin</button>
                 {/* <button variant="outline-info">Info</button>{' '} */}
                 {/* <button buttonSize='btn--wide' buttonColor='blue'> {buttonLabel}
