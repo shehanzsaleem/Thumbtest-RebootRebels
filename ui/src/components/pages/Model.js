384e7ff4bb38e1fc5d8ee4d9bb64e36229d963f4
zlogonje-pissa <56260108+shehanzsaleem@users.noreply.github.com>
2021-03-17 19:58:16 +0530

    Update

--- ui/src/components/pages/Model.js
+++ ui/src/components/pages/Model.js
@@ -72,47 +72,47 @@ function model() {
                                 alert(`Cannot upload files more than ${MAX_LENGTH}`);
                                 return;
                             }
-                            else{
+                            else {
 
 
 
-                            console.log("Images Selected...");
+                                console.log("Images Selected...");
 
 
-                            let reader0 = new FileReader();
-                            let file0 = $("#image-selector").prop('files')[0];
-                            reader0.readAsDataURL(file0);
-                            reader0.onload = function () {
-                                let dataURL = reader0.result;
-                                $("#selected-image0").attr("src", dataURL);
-                                $("#prediction-list0").empty();
+                                let reader0 = new FileReader();
+                                let file0 = $("#image-selector").prop('files')[0];
+                                reader0.readAsDataURL(file0);
+                                reader0.onload = function () {
+                                    let dataURL = reader0.result;
+                                    $("#selected-image0").attr("src", dataURL);
+                                    $("#prediction-list0").empty();
+                                }
+
+
+                                let reader1 = new FileReader();
+                                let file1 = $("#image-selector").prop('files')[1];
+                                reader1.readAsDataURL(file1);
+                                reader1.onload = function () {
+                                    let dataURL = reader1.result;
+                                    $("#selected-image1").attr("src", dataURL);
+                                    $("#prediction-list1").empty();
+                                }
+
+
+                                let reader2 = new FileReader();
+                                let file2 = $("#image-selector").prop('files')[2];
+                                reader2.readAsDataURL(file2);
+                                reader2.onload = function () {
+                                    let dataURL = reader2.result;
+                                    $("#selected-image2").attr("src", dataURL);
+                                    $("#prediction-list2").empty();
+                                }
+
+
+
+
+
                             }
-
-
-                            let reader1 = new FileReader();
-                            let file1 = $("#image-selector").prop('files')[1];
-                            reader1.readAsDataURL(file1);
-                            reader1.onload = function () {
-                                let dataURL = reader1.result;
-                                $("#selected-image1").attr("src", dataURL);
-                                $("#prediction-list1").empty();
-                            }
-
-
-                            let reader2 = new FileReader();
-                            let file2 = $("#image-selector").prop('files')[2];
-                            reader2.readAsDataURL(file2);
-                            reader2.onload = function () {
-                                let dataURL = reader2.result;
-                                $("#selected-image2").attr("src", dataURL);
-                                $("#prediction-list2").empty();
-                            }
-
-
-                         
-
-
-                        }
 
 
 
@@ -139,35 +139,43 @@ function model() {
                             console.log("Button Clicked...");
 
 
-                            for(let i = 0; i < 3 ; i ++){
+                            for (let i = 0; i < 3; i++) {
 
-                            let image = $('#selected-image'+i).get(0);
+                                let image = $('#selected-image' + i).get(0);
 
-                            console.log("Selected Image Loaded...");
-                            // Pre-processing the image
-                            let tensor = tf.browser.fromPixels(image)
-                                .resizeNearestNeighbor([96, 96]) // the image is reduced size here
-                                .toFloat()
-                                .div(tf.scalar(255.0))
-                                .expandDims();
-                            console.log("Image PreProcessed...");
-                            let predictions = await modeljson.predict(tensor).data();
-                            let top5 = Array.from(predictions)
-                                .map(function (p, i) { // this is Array.map
-                                    return {
-                                        probability: p,
-                                        className: TARGET_CLASSES[i] // we are selecting the value from the obj
-                                    };
-                                }).sort(function (a, b) {
-                                    return b.probability - a.probability;
-                                }).slice(0, 2);
-                            $("#prediction-list"+i).empty();
-                            top5.forEach(function (p) {
-                                $("#prediction-list"+i).append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
-                            });
+                                console.log("Selected Image Loaded...");
+                                // Pre-processing the image
+                                let tensor = tf.browser.fromPixels(image)
+                                    .resizeNearestNeighbor([96, 96]) // the image is reduced size here
+                                    .toFloat()
+                                    .div(tf.scalar(255.0))
+                                    .expandDims();
+                                console.log("Image PreProcessed...");
+                                let predictions = await modeljson.predict(tensor).data();
+                                let top5 = Array.from(predictions)
+                                    .map(function (p, i) { // this is Array.map
+                                        return {
+                                            probability: p,
+                                            className: TARGET_CLASSES[i] // we are selecting the value from the obj
+                                        };
+                                    }).sort(function (a, b) {
+                                        // return b.probability - a.probability;
+                                        return a.probability;
+                                    }).slice(1, 2);
+                                $("#prediction-list" + i).empty();
+                                $("#prediction-percentage" + i).empty();
+                                top5.forEach(function (p) {
 
 
-                        }
+                                    // $("#prediction-list"+i).append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
+
+                                    $("#prediction-percentage" + i).append(`${(p.probability * 100).toFixed(2)}%`);
+
+
+                                });
+
+
+                            }
 
 
                         }
@@ -178,36 +186,33 @@ function model() {
             <br />
             <br />
             <div className='row'>
-            <div className='col-12'>
+                <div className='col-12'>
                     <h2 className=' '>Thumbnail</h2>
-                    </div>
+                </div>
                 <div className='col-4'>
-                   
+
                     {/* <img id='selected-image0' className=' ' width='500' alt='' /> */}
                     <h2 className=' '>Score1</h2>
-                    <ol id='prediction-list0'>
-                    </ol>
-                    </div>
+
+                </div>
                 <div className='col-4'>
-                    <img id='selected-image1' className=' ' width='500' alt='' />
+                    {/* <img id='selected-image1' className=' ' width='500' alt='' /> */}
                     <h2 className=' '>Score2</h2>
-                    <ol id='prediction-list1'>
-                    </ol>
-                    </div>
+
+                </div>
                 <div className='col-4'>
-                    <img id='selected-image2' className=' ' width='500' alt='' />
+                    {/* <img id='selected-image2' className=' ' width='500' alt='' /> */}
                     <h2 className=' '>Score3</h2>
-                    <ol id='prediction-list2'>
-                    </ol>
+
                 </div>
                 <div className='col-12'>
-                 
+
                 </div>
             </div>
             <div className='row'>
                 <div className='col-12'>
 
-                    <h2 className=' '>Your Scores</h2>
+                    {/* <h2 className=' '>Your Scores</h2> */}
                 </div>
 
 
@@ -216,11 +221,17 @@ function model() {
 
                         <div className='score__container-cardInfo'>
                             <div className='icon'>
- <FaFire/>
- </div>
+                                <FaFire />
+                            </div>
                             <h3>Second </h3>
-                            <h4>76.80%</h4>
+                            <ol id='prediction-list0'>
+                            </ol>
+                            <h4 id='prediction-percentage0'></h4>
+                            <div class="containerp">
+                           
+                            <div className='image-wrapper'>
                             <img id="selected-image0" className="score_second" /*src="./sample_thumbnail_01"*/ />
+                            </div>  </div>
                             {/* <p>per month</p>
  <ul className='score__container-features'>
  <li>100 Transactions</li>
@@ -238,11 +249,18 @@ function model() {
 
                         <div className='score__container-cardInfo'>
                             <div className='icon'>
- <BsXDiamondFill/>
- </div>
+                                <BsXDiamondFill />
+                            </div>
                             <h3>Best</h3>
-                            <h4>95.00%</h4>
+                            <ol id='prediction-list1'>
+                            </ol>
+
+                            <h4 id='prediction-percentage1'></h4>
+                            <div class="containerp">
+                            <div className='image-wrapper'>
                             <img id="selected-image1" className="score_best" /*src="./sample_thumbnail_01"*/ />
+                            </div>
+                            </div>
                             {/* <p>per month</p>
  <ul className='score__container-features'>
  <li>1000 Transactions</li>
@@ -259,11 +277,18 @@ function model() {
                     <div className='score__container-card'>
                         <div className='score__container-cardInfo'>
                             <div className='icon'>
- <GiCrystalize/>
- </div>
+                                <GiCrystalize />
+                            </div>
                             <h3>Third</h3>
-                            <h4>80.08%</h4>
+                            <ol id='prediction-list2'>
+                            </ol>
+
+                            <h4 id='prediction-percentage2'></h4>
+                            <div class="containerp">
+                           
+<div className='image-wrapper'>
                             <img id="selected-image2" className="score_third" /*src="./sample_thumbnail_01"*/ />
+                            </div></div>
                             {/* <div className="score_third">
 </div> */}
                             {/* <p>per month</p>
