1c024aa26e90ef9bfadee704addf13c89fd233da
samithakalpana <66740752+samithakalpana@users.noreply.github.com>
2021-03-19 17:15:25 +0530

    login functions

--- ui/src/components/layouts/Login.js
+++ ui/src/components/layouts/Login.js
@@ -1,18 +1,107 @@
-import React from 'react';
+import React, { useState, useCallback,useEffect } from 'react';
 import "./Login.css";
-const Login = (props) => {
-    const {
-        email,
-        setEmail,
-        password,
-        setPassword,
-        handleLogin,
-        handleSignup,
-        hasAccount,
-        sethasAccount,
-        emailError,
-        passwordError
-    } = props;
+import fire from './fire';
+const Login = () => {
+
+
+
+
+
+    const [user, setUser] = useState('');
+    const [email, setEmail] = useState('');
+    const [password , setPassword] = useState('');
+    const [emailError , setEmaiError] = useState('');
+    const  [passwordError , setPasswordError] = useState('');
+    const [hasAccount, setHasAccount] = useState('');
+  
+  const clearInputs = ()=>{
+    setEmail('');
+    setPassword('');
+  }  
+  
+  const clearErrors = () => {
+    setEmaiError('');
+    setPasswordError('');
+  }
+  
+  const handleLogin = () =>
+  {
+    clearErrors();
+    fire
+  
+    .auth()
+    .signInWithEmailAndPassword(email , password)
+    .catch((err)=>{
+      switch (err.code){
+        case "auth/invalid-emai":
+        case "auth/user-disabled":
+        case "suth/user-not-found":
+          setEmaiError(err.message);
+          break;
+        case "auth/wrong-password":
+          setPasswordError(err.message);
+          break;
+  
+      }
+    });
+  };
+  
+  const handleSignup = () =>
+  {
+    clearErrors();
+      fire
+    .auth()
+    .createUserWithEmailAndPassword(email , password)
+    .catch((err)=>{
+      switch (err.code){
+        case "auth/email-already-in-use":
+        case "auth/invalid-email":
+        
+          setEmaiError(err.message);
+          break;
+        case "auth/weak-password":
+          setPasswordError(err.message);
+          break;
+  
+      }
+    });
+  }
+  
+  const handleLogout = () =>{
+  
+    fire.auth().signOut();
+  };
+  
+  
+  const authListener = () =>{
+    fire.auth().onAuthStateChanged((user) =>{
+      if(user){
+        clearInputs();
+        setUser(user);
+  
+      }else{
+        setUser("");
+      }
+  
+    });
+  };
+  
+  useEffect (() =>{
+    authListener();
+  },[]);
+  
+    // const {
+    //     email,
+    //     setEmail,
+    //     password,
+    //     setPassword,
+    //     handleLogin,
+    //     handleSignup,
+    //     hasAccount,
+    //     sethasAccount,
+    //     emailError,
+    //     passwordError
+    // } = props;
     // const {email} = props;
     // const {setEmail} = props;
     // const {password} = props;
@@ -26,32 +115,32 @@ const Login = (props) => {
     return (
 
 
-        <div className="container-fluid login">
-            <div className="row">
-                <div className="col-md-6 col-sm-12 mt-5 mb-5 pt-5 pb-5">
-                    <div className="login-main-text">
-                        <h2>Application<br /> Login Page</h2>
-                        <p>Login or register from here to access.</p>
-                    </div>
-                </div>
-                <div className="col-md-6 col-sm-12 mt-5 mb-5 pt-5 pb-5">
-                    <div className="login-form">
-                        <form>
-                            <div className="form-group">
-                                <label>User Name</label>
-                                <input type="text" className="form-control" placeholder="User Name" />
-                            </div>
-                            <div className="form-group">
-                                <label>Password</label>
-                                <input type="password" className="form-control" placeholder="Password" />
-                            </div>
-                            <button type="submit" className="btn btn-black">Login</button>
-                            <button type="submit" className="btn btn-secondary">Register</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-       
+        // <div className="container-fluid login">
+        //     <div className="row">
+        //         <div className="col-md-6 col-sm-12 mt-5 mb-5 pt-5 pb-5">
+        //             <div className="login-main-text">
+        //                 <h2>Application<br /> Login Page</h2>
+        //                 <p>Login or register from here to access.</p>
+        //             </div>
+        //         </div>
+        //         <div className="col-md-6 col-sm-12 mt-5 mb-5 pt-5 pb-5">
+        //             <div className="login-form">
+        //                 <form>
+        //                     <div className="form-group">
+        //                         <label>User Name</label>
+        //                         <input type="text" className="form-control" placeholder="User Name" />
+        //                     </div>
+        //                     <div className="form-group">
+        //                         <label>Password</label>
+        //                         <input type="password" className="form-control" placeholder="Password" />
+        //                     </div>
+        //                     <button type="submit" className="btn btn-black">Login</button>
+        //                     <button type="submit" className="btn btn-secondary">Register</button>
+        //                 </form>
+        //             </div>
+        //         </div>
+        //     </div>
+        <div>
         <section classNameName="login" >
         <div classNameName="loginContainer">
         </div> <label>Username</label>
