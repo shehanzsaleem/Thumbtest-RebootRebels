7d9171e10fe9dae2500211d48b9259aed89520b4
zlogonje-pissa <56260108+shehanzsaleem@users.noreply.github.com>
2021-03-19 17:20:22 +0530

    Update

--- ui/src/components/layouts/Login.js
+++ ui/src/components/layouts/Login.js
@@ -120,60 +120,52 @@ const Login = () => {
     return (
 
 
-        // <div className="container-fluid login">
-        //     <div className="row">
-        //         <div className="col-md-6 col-sm-12 mt-5 mb-5 pt-5 pb-5">
-        //             <div className="login-main-text">
-        //                 <h2>Application<br /> Login Page</h2>
-        //                 <p>Login or register from here to access.</p>
-        //             </div>
-        //         </div>
-        //         <div className="col-md-6 col-sm-12 mt-5 mb-5 pt-5 pb-5">
-        //             <div className="login-form">
-        //                 <form>
-        //                     <div className="form-group">
-        //                         <label>User Name</label>
-        //                         <input type="text" className="form-control" placeholder="User Name" />
-        //                     </div>
-        //                     <div className="form-group">
-        //                         <label>Password</label>
-        //                         <input type="password" className="form-control" placeholder="Password" />
-        //                     </div>
-        //                     <button type="submit" className="btn btn-black">Login</button>
-        //                     <button type="submit" className="btn btn-secondary">Register</button>
-        //                 </form>
-        //             </div>
-        //         </div>
-        //     </div>
-        <div>
-            <section classNameName="login" >
-                <div classNameName="loginContainer">
-                </div> <label>Username</label>
-                <input type="text" autoFocus required value={email} onChange={(e) => setEmail(e.target.value)}
-                />
-                <p classNameName="errorMsg">{emailError}</p>
-                <label>Password</label>
-                <input type="password"
-                    required
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)} />
-                <p classNameName="errorMsg">{passwordError}</p>
-                <div className="btnContainer">
-                    {hasAccount ? (
-                        <>
-                            <button classNameName="b1" onClick={handleLogin}>Sign in</button>
-                            <p>Don't have an account ? <span onClick={() => sethasAccount(!hasAccount)}>Sign up</span></p>
-                        </>
-                    ) : (
-                        <>
-                            <button classNameName="b1" onClick={handleSignup}>Sign up</button>
-                            <p>Have an account ? <span onClick={() => sethasAccount(!hasAccount)}>Sign in</span></p>
-                        </>
-                    )
-                    }
+        <div className="container-fluid">
+        <div className="row">
+            <div className="col-md-6 col-sm-12 mt-5 mb-5 pt-5 pb-5">
+                <div className="login-main-text">
+                    <h2>Application<br /> Login Page</h2>
+                    <p>Login or register from here to access.</p>
                 </div>
-            </section>
+            </div>
+            <div className="col-md-6 col-sm-12 mt-5 mb-5 pt-5 pb-5">
+                <div className="login-form">
+                    <form>
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input type="email" autoFocus required value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="User Name" required />
+                            <p classNameName="errorMsg">{emailError}</p>
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="password" className="form-control" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+                            <p classNameName="errorMsg">{passwordError}</p>
+                        </div>
+                        <div className="btnContainer">
+                            {hasAccount ? (
+                                <>
+                                    <button type="submit" className="btn btn-black" onClick={handleLogin}>Sign in</button>
+                                    <p>Don't have an account ? <span onClick={() => sethasAccount(!hasAccount)}>Sign up</span></p>
+                                </>
+                            ) : (
+                                <>
+                                    <button type="submit" className="btn btn-secondary" onClick={handleSignup}>Sign up</button>
+                                    <p>Have an account ? <span onClick={() => sethasAccount(!hasAccount)}>Sign in</span></p>
+                                </>
+                            )
+                            }
+                        </div>
+
+                    </form>
+                </div>
+            </div>
         </div>
+
+    </div>
+
+
+
+
     );
 };
 export default Login;
