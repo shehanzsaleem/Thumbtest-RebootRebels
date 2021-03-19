53962a913348d6ada8259b5d2588fee781c98ed0
zlogonje-pissa <56260108+shehanzsaleem@users.noreply.github.com>
2021-03-19 17:37:15 +0530

    Update Login.js

--- ui/src/components/layouts/Login.js
+++ ui/src/components/layouts/Login.js
@@ -2,32 +2,29 @@ import React, { useState, useCallback, useEffect } from 'react';
 import "./Login.css";
 import fire from './fire';
 const Login = () => {
-
-
-
-
-
     const [user, setUser] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [emailError, setEmaiError] = useState('');
     const [passwordError, setPasswordError] = useState('');
     const [hasAccount, setHasAccount] = useState('');
-
     const clearInputs = () => {
+        
+    console.log("clear inputs");
         setEmail('');
         setPassword('');
     }
-
     const clearErrors = () => {
+        
+    console.log("clear errors");
         setEmaiError('');
         setPasswordError('');
     }
-
     const handleLogin = () => {
+        
+    console.log("handle login");
         clearErrors();
         fire
-
             .auth()
             .signInWithEmailAndPassword(email, password)
             .catch((err) => {
@@ -40,12 +37,12 @@ const Login = () => {
                     case "auth/wrong-password":
                         setPasswordError(err.message);
                         break;
-
                 }
             });
     };
-
     const handleSignup = () => {
+        
+    console.log("handle signup");
         clearErrors();
         fire
             .auth()
@@ -54,46 +51,42 @@ const Login = () => {
                 switch (err.code) {
                     case "auth/email-already-in-use":
                     case "auth/invalid-email":
-
                         setEmaiError(err.message);
                         break;
                     case "auth/weak-password":
                         setPasswordError(err.message);
                         break;
-
                 }
             });
     }
-
-
-
     const sethasAccount = () => {
-
+        
+    console.log("set has account");
        
     };
-
     const handleLogout = () => {
-
+        
+    console.log("handle logout");
         fire.auth().signOut();
     };
-
-
     const authListener = () => {
+        
+    console.log("auth listener");
         fire.auth().onAuthStateChanged((user) => {
             if (user) {
                 clearInputs();
                 setUser(user);
-
             } else {
                 setUser("");
             }
-
         });
     };
+
 
     useEffect(() => {
         authListener();
     }, []);
+
 
     // const {
     //     email,
@@ -118,8 +111,6 @@ const Login = () => {
     // const {emailError} = props;
     // const {passwordError} = props;
     return (
-
-
         <div className="container-fluid">
         <div className="row">
             <div className="col-md-6 col-sm-12 mt-5 mb-5 pt-5 pb-5">
@@ -134,12 +125,12 @@ const Login = () => {
                         <div className="form-group">
                             <label>Email</label>
                             <input type="email" autoFocus required value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="User Name" required />
-                            <p classNameName="errorMsg">{emailError}</p>
+                            <p className="errorMsg">{emailError}</p>
                         </div>
                         <div className="form-group">
                             <label>Password</label>
                             <input type="password" className="form-control" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)} />
-                            <p classNameName="errorMsg">{passwordError}</p>
+                            <p className="errorMsg">{passwordError}</p>
                         </div>
                         <div className="btnContainer">
                             {hasAccount ? (
@@ -155,17 +146,11 @@ const Login = () => {
                             )
                             }
                         </div>
-
                     </form>
                 </div>
             </div>
         </div>
-
     </div>
-
-
-
-
     );
 };
 export default Login;
