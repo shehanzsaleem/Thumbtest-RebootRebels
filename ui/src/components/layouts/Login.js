305b0d61cc955f526b8fca05a0df773fd3eb15cb
zlogonje-pissa <56260108+shehanzsaleem@users.noreply.github.com>
2021-03-19 17:11:47 +0530

    Update

--- ui/src/components/layouts/Login.js
+++ ui/src/components/layouts/Login.js
@@ -24,9 +24,7 @@ const Login = (props) => {
     // const {emailError} = props;
     // const {passwordError} = props;
     return (
-
-
-        <div className="container-fluid login">
+        <div className="container-fluid">
             <div className="row">
                 <div className="col-md-6 col-sm-12 mt-5 mb-5 pt-5 pb-5">
                     <div className="login-main-text">
@@ -38,47 +36,35 @@ const Login = (props) => {
                     <div className="login-form">
                         <form>
                             <div className="form-group">
-                                <label>User Name</label>
-                                <input type="text" className="form-control" placeholder="User Name" />
+                                <label>Email</label>
+                                <input type="email" autoFocus required value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="User Name" required />
+                                <p classNameName="errorMsg">{emailError}</p>
                             </div>
                             <div className="form-group">
                                 <label>Password</label>
-                                <input type="password" className="form-control" placeholder="Password" />
+                                <input type="password" className="form-control" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+                                <p classNameName="errorMsg">{passwordError}</p>
                             </div>
-                            <button type="submit" className="btn btn-black">Login</button>
-                            <button type="submit" className="btn btn-secondary">Register</button>
+                            <div className="btnContainer">
+                                {hasAccount ? (
+                                    <>
+                                        <button type="submit" className="btn btn-black" onClick={handleLogin}>Sign in</button>
+                                        <p>Don't have an account ? <span onClick={() => sethasAccount(!hasAccount)}>Sign up</span></p>
+                                    </>
+                                ) : (
+                                    <>
+                                        <button type="submit" className="btn btn-secondary" onClick={handleSignup}>Sign up</button>
+                                        <p>Have an account ? <span onClick={() => sethasAccount(!hasAccount)}>Sign in</span></p>
+                                    </>
+                                )
+                                }
+                            </div>
+
                         </form>
                     </div>
                 </div>
             </div>
-       
-        <section classNameName="login" >
-        <div classNameName="loginContainer">
-        </div> <label>Username</label>
-        <input type="text" autoFocus required value={email} onChange={(e) => setEmail(e.target.value)}
-        />
-        <p classNameName="errorMsg">{emailError}</p>
-        <label>Password</label>
-        <input type="password"
-        required
-        value={password}
-        onChange={(e) => setPassword(e.target.value)} />
-        <p classNameName="errorMsg">{passwordError}</p>
-        <div className="btnContainer">
-        {hasAccount ? (
-        <>
-        <button classNameName="b1" onClick={handleLogin}>Sign in</button>
-        <p>Don't have an account ? <span onClick={() => sethasAccount(!hasAccount)}>Sign up</span></p>
-        </>
-        ) : (
-        <>
-        <button classNameName="b1" onClick={handleSignup}>Sign up</button>
-        <p>Have an account ? <span onClick={() => sethasAccount(!hasAccount)}>Sign in</span></p>
-        </>
-        )
-        }
-        </div>
-        </section>
+
         </div>
     );
 };
